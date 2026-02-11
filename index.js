2d934dfc37643c514c45dac57f5a5093ad272270
itmanbanana <itmanbanana@gmail.com>
2026-02-10 21:45:19 -0500

    Add backend tic tac toe game logic.
    Uses IIFE modules for game manager and board objects.

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-const validStates: string = "XO";
-
-type BoardType = [[string, string, string],
-    [string, string, string],
-    [string, string, string]];
-
-type BoardIndex = 0 | 1 | 2;
-
+const validStates = "XO";
+const movePositions = "012";
+class Player {
+    _turn;
+    _name;
+    constructor(turn = false, name) {
+        this._turn = turn;
+        this._name = name;
+    }
+    get turn() { return this._turn; }
+    get name() { return this._name; }
+    toggleTurn() { this._turn = !this._turn; }
+}
 const board = (() => {
-    let boardState: BoardType = [["", "", ""],
-    ["", "", ""],
-    ["", "", ""]];
-
-    const getBoardState = (x: BoardIndex, y: BoardIndex) => boardState[x][y];
-
-    const setBoardState = (x: BoardIndex, y: BoardIndex, s: string) => {
+    let boardState = [["", "", ""],
+        ["", "", ""],
+        ["", "", ""]];
+    const getBoardState = (x, y) => boardState[x][y];
+    const setBoardState = (x, y, s) => {
         if (x >= 0 && x <= 2 && y >= 0 && y <= 2 && validStates.includes(s)
             && boardState[x][y] !== undefined) {
             boardState[x][y] = s;
         }
     };
-
     const resetBoardState = () => {
         boardState = [["", "", ""],
-        ["", "", ""],
-        ["", "", ""]];
+            ["", "", ""],
+            ["", "", ""]];
     };
-
-    const checkWin = (s: string): boolean => {
-        if (!validStates.includes(s)) return false;
-
-        let i: BoardIndex;
+    const checkWin = (s) => {
+        if (!validStates.includes(s))
+            return false;
+        let i;
         for (i = 0; i < 3; i++) {
             if ((boardState[0][i] == s && boardState[1][i] == s && boardState[2][i] == s) ||
                 (boardState[i]?.[0] == s && boardState[i]?.[1] == s && boardState[i]?.[2] == s) ||
@@ -38,64 +39,63 @@ const board = (() => {
                 return true;
             }
         }
-
         return false;
     };
-
-    const checkTie = (): boolean => {
-        for (let x: BoardIndex = 0; x < 3; x++) {
-            for (let y: BoardIndex = 0; y < 3; y++) {
-                if (boardState?.[x]?.[y] === "") return false;
+    const checkTie = () => {
+        for (let x = 0; x < 3; x++) {
+            for (let y = 0; y < 3; y++) {
+                if (boardState?.[x]?.[y] === "")
+                    return false;
             }
         }
         return true;
-    }
-
+    };
     return { resetBoardState, getBoardState, setBoardState, checkWin, checkTie };
 })();
-
 const gameManager = (() => {
+    // let player1: Player = new Player(true, "X");
+    // let player2: Player = new Player(false, "O");
     let currentTurn = "X";
-
     const startGame = () => {
         board.resetBoardState();
         takeTurn();
     };
-
     const takeTurn = () => {
         let input;
         let move;
         let x;
         let y;
-
         while (x === undefined || y === undefined || x < 0 || x > 2 || y < 0 || y > 2) {
             input = prompt(`${currentTurn}'s turn. Type board coordinates:`);
-            if (input === '') continue
-
+            if (input === '')
+                continue;
             move = input?.replace(/[^0-2]/g, '');
             x = Number(move?.[0]);
             y = Number(move?.[1]);
         }
-
-        let boardX: BoardIndex = x as BoardIndex;
-        let boardY: BoardIndex = y as BoardIndex;
+        let boardX = x;
+        let boardY = y;
         let boardPositionState = board.getBoardState(boardX, boardY);
-
         if (boardPositionState !== undefined && boardPositionState === "") {
             board.setBoardState(boardX, boardY, currentTurn);
             let isWin = board.checkWin(currentTurn);
             let isTie = board.checkTie();
-
-            if (isWin) { alert(`${currentTurn} wins!`); return; }
-            if (isTie) { alert("It's a tie!"); return; }
-
+            if (isWin) {
+                alert(`${currentTurn} wins!`);
+                return;
+            }
+            if (isTie) {
+                alert("It's a tie!");
+                return;
+            }
             currentTurn = (currentTurn === "X") ? "O" : "X";
-        } else alert("Move invalid");
-
+        }
+        else
+            alert("Move invalid");
         takeTurn();
-    }
-
-    return { startGame }
+    };
+    return { startGame };
 })();
-
 gameManager.startGame();
+export {};
+//# sourceMappingURL=index.js.map
